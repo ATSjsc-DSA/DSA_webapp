612332b646269114e45ea89c538d371d8ccbd407
DuyNV <nvd.bka@gmail.com>
2024-08-09 15:02:23 +0700

    add gitignore index.js

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = !import.meta.env.MODE || import.meta.env.MODE === 'development' ? 'http://localhost:9000' : '';
+export const BASE_URL = !import.meta.env.MODE || import.meta.env.MODE === 'development' ? 'http://localhost:8000' : '';
 export const intervalTime = 5000;
